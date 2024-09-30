d1b84cf799d865f401e2ddffb59df37cabbd229c
Mauro Soares <mauro@tempocerto.inf.br>
2024-09-29 23:23:42 -0300

    :sparkles: 5

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 exports.cadastrarProduto = async (req, res) => {
     const { descricao, codigo_sequencial, status, estoque, valor } = req.body
-    const {imagens, codigo_de_barras} = req.files
+    const { imagens, codigo_de_barras } = req.files
     let imagem_list = []
     for (imagem of imagens) {
         imagem_list.push(imagem.buffer)
@@ -11,21 +11,22 @@ exports.cadastrarProduto = async (req, res) => {
 
     try {
         const produto = new produtoModel({
-            descricao,
-            codigo_sequencial,
-            codigo_de_barras: codigo_de_barras[0].buffer,
-            status, 
-            estoque,
-            valor,
-            imagens: imagem_list
+            descricao: descricao != "undefined" ? descricao : oldProduto.descricao,
+            codigo_sequencial: codigo_sequencial != "undefined" ? codigo_sequencial : oldProduto.codigo_sequencial,
+            codigo_de_barras: codigo_de_barras ? codigo_de_barras[0].buffer : oldProduto.codigo_de_barras,
+            status: status != "undefined" ? status : (estoque > 0 ? 'ativo' : oldProduto.status),
+            estoque: estoque != "undefined" ? parseInt(estoque) : parseInt(oldProduto.estoque),
+            valor: valor != "undefined" ? valor : oldProduto.valor,
+            imagens: imagem_list.length > 0 ? imagem_list : oldProduto.imagens
+
         })
-    
+
         await produto.save()
-    
+
         res.status(201).send(produto);
     } catch (err) {
         console.log(err)
-        return res.status(500).send({error: err})
+        return res.status(500).send({ error: err })
     }
 };
 
@@ -34,7 +35,7 @@ exports.subtrairEstoque = async (req, res) => {
     let produtoToUpdate
     try {
         for (let produto of carrinho) {
-            produtoToUpdate = await produtoModel.findOne({_id: new mongoose.Types.ObjectId(produto.produto._id)})
+            produtoToUpdate = await produtoModel.findOne({ _id: new mongoose.Types.ObjectId(produto.produto._id) })
             produtoToUpdate.estoque -= produto.quantidade
             if (produtoToUpdate.estoque <= 0) {
                 produtoToUpdate.status = 'inativo'
@@ -46,14 +47,14 @@ exports.subtrairEstoque = async (req, res) => {
         return res.status(201).send(produtoToUpdate)
     } catch (err) {
         console.log(err)
-        return res.status(500).send({error: err})
+        return res.status(500).send({ error: err })
     }
 };
 
 exports.editarProduto = async (req, res) => {
     const { produtoId } = req.params
     const { descricao, codigo_sequencial, status, estoque, valor } = req.body
-    const {imagens, codigo_de_barras} = req.files
+    const { imagens, codigo_de_barras } = req.files
     console.log(req.body)
 
     let imagem_list = []
@@ -62,53 +63,53 @@ exports.editarProduto = async (req, res) => {
             imagem_list.push(imagem.buffer)
         }
     }
-    const oldProduto = await produtoModel.findOne({_id: new mongoose.Types.ObjectId(produtoId)})
+    const oldProduto = await produtoModel.findOne({ _id: new mongoose.Types.ObjectId(produtoId) })
     try {
         const produto = await produtoModel.findOneAndUpdate(
-            {_id: new mongoose.Types.ObjectId(produtoId)},
+            { _id: new mongoose.Types.ObjectId(produtoId) },
             {
                 descricao: descricao != "undefined" ? descricao : oldProduto.descricao,
-                codigo_sequencial: codigo_sequencial != "undefined"  ? codigo_sequencial : oldProduto.codigo_sequencial,
+                codigo_sequencial: codigo_sequencial != "undefined" ? codigo_sequencial : oldProduto.codigo_sequencial,
                 codigo_de_barras: codigo_de_barras ? codigo_de_barras[0].buffer : oldProduto.codigo_de_barras,
-                status: status  != "undefined" ? status : (estoque > 0 ? 'ativo' : oldProduto.status), 
+                status: status != "undefined" ? status : (estoque > 0 ? 'ativo' : oldProduto.status),
                 estoque: estoque != "undefined" ? parseInt(estoque) : parseInt(oldProduto.estoque),
                 valor: valor != "undefined" ? valor : oldProduto.valor,
-                imagens: imagem_list.length > 0 ? imagem_list : oldProduto.imagens 
+                imagens: imagem_list.length > 0 ? imagem_list : oldProduto.imagens
             },
-            {new: true}
+            { new: true }
         )
         return res.status(201).send(produto);
     } catch (err) {
         console.log(err)
-        return res.status(500).send({error: err})
+        return res.status(500).send({ error: err })
     }
 };
 
 exports.desativarProduto = async (req, res) => {
     const { produtoId } = req.params
-   
+
     try {
-        const usuario = await produtoModel.findOneAndUpdate({_id: new mongoose.Types.ObjectId(produtoId)}, 
-        {status: 'inativo'})
+        const usuario = await produtoModel.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(produtoId) },
+            { status: 'inativo' })
 
         res.status(201).send("Produto desativado com sucesso!");
     } catch (err) {
         console.log(err)
-        return res.status(500).send({error: err})
+        return res.status(500).send({ error: err })
     }
 };
 
 exports.ativarProduto = async (req, res) => {
     const { produtoId } = req.params
-   
+
     try {
-        const usuario = await produtoModel.findOneAndUpdate({_id: new mongoose.Types.ObjectId(produtoId)}, 
-        {status: 'ativo'})
+        const usuario = await produtoModel.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(produtoId) },
+            { status: 'ativo' })
 
         res.status(201).send("Produto ativado com sucesso!");
     } catch (err) {
         console.log(err)
-        return res.status(500).send({error: err})
+        return res.status(500).send({ error: err })
     }
 };
 
@@ -119,7 +120,7 @@ exports.listarProdutos = async (req, res) => {
         res.status(201).send(produto);
     } catch (err) {
         console.log(err)
-        return res.status(500).send({error: err})
+        return res.status(500).send({ error: err })
     }
 };
 
