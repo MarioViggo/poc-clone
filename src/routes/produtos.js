eeaf20010406c5c40e80d1b2f111c06556509004
unknown <junior.soares@viggosistemas.com.br>
2024-10-01 16:12:35 -0300

    :construction:

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -8,5 +8,5 @@ router.get('/listarProdutos',  controller.listarProdutos);
 router.post('/desativarProduto/:produtoId',  controller.desativarProduto);
 router.post('/ativarProduto/:produtoId',  controller.ativarProduto);
 router.post('/subtrairEstoque', controller.subtrairEstoque);
-router.post('/editarProduto/:produtoId', controller.editarProduto)
+router.post('/editarProduto/:produto', controller.editarProduto)
 module.exports = router;
